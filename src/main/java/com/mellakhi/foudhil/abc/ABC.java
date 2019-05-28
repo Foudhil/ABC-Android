428909f44e26e8268001a320237bd335b43f456f
Foudhil <48635053+Foudhil@users.noreply.github.com>
2019-05-28 23:40:20 +0100

    Add files via upload

---
+++ src/main/java/com/mellakhi/foudhil/abc/ABC.java
@@ -0,0 +1,19 @@
+package com.mellakhi.foudhil.abc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ABC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        //TODO ABC && 123
+        //array of imageView
+        //each element lead to new activity
+        //next  && back btn synced with array
+
+    }
+}
---
+++ src/main/java/com/mellakhi/foudhil/abc/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mellakhi.foudhil.abc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TODO
+        //btn act -> abc && 123 && abc song && test
+        //song
+        //
+    }
+}
