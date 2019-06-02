0dadea5d79973176a07e07c7e631db810fb56c1a
Merah Soheyb <soheybemir@hotmail.fr>
2019-06-02 22:01:50 +0100

    Update SelectedImage.java

--- src/main/java/com/mellakhi/foudhil/abc/SelectedImage.java
+++ src/main/java/com/mellakhi/foudhil/abc/SelectedImage.java
@@ -4,6 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SelectedImage extends AppCompatActivity {
+    
+    //TODO
+    // ADD in XML
+    /*
+    RelativeLayout relativeLayout;
+    Paint paint;
+    View view;
+    Path path2;
+    Bitmap bitmap;
+    Canvas canvas;
+    Button button;
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +25,120 @@ public class SelectedImage extends AppCompatActivity {
         //TODO
         //invoked when onArrayItemClick
         //synced with bundle && intent
+        
+        // PAINTING
+        relativeLayout = findViewById(R.id.relativelayout1);
+
+        button = (Button)findViewById(R.id.button);
+
+        view = new SketchSheetView(MainActivity.this);
+
+        paint = new Paint();
+
+        path2 = new Path();
+
+        relativeLayout.addView(view, new LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT));
+
+        paint.setDither(true);
+
+        paint.setColor(Color.parseColor("#000000"));
+
+        paint.setStyle(Paint.Style.STROKE);
+
+        paint.setStrokeJoin(Paint.Join.ROUND);
+
+        paint.setStrokeCap(Paint.Cap.ROUND);
+
+        paint.setStrokeWidth(20);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                path2.reset();
+
+            }
+        });
+        
     }
+    
+        class SketchSheetView extends View {
+
+        public SketchSheetView(Context context) {
+
+            super(context);
+
+            bitmap = Bitmap.createBitmap(820, 480, Bitmap.Config.ARGB_4444);
+
+            canvas = new Canvas(bitmap);
+
+            this.setBackgroundColor(Color.WHITE);
+        }
+
+        private ArrayList<DrawingClass> DrawingClassArrayList = new ArrayList<DrawingClass>();
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+
+            DrawingClass pathWithPaint = new DrawingClass();
+
+            canvas.drawPath(path2, paint);
+
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                path2.moveTo(event.getX(), event.getY());
+
+                path2.lineTo(event.getX(), event.getY());
+            }
+            else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+
+                path2.lineTo(event.getX(), event.getY());
+
+                pathWithPaint.setPath(path2);
+
+                pathWithPaint.setPaint(paint);
+
+                DrawingClassArrayList.add(pathWithPaint);
+            }
+
+            invalidate();
+            return true;
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            if (DrawingClassArrayList.size() > 0) {
+
+                canvas.drawPath(
+                        DrawingClassArrayList.get(DrawingClassArrayList.size() - 1).getPath(),
+
+                        DrawingClassArrayList.get(DrawingClassArrayList.size() - 1).getPaint());
+            }
+        }
+    }
+
+    public class DrawingClass {
+        Path DrawingClassPath;
+        Paint DrawingClassPaint;
+
+        public Path getPath() {
+            return DrawingClassPath;
+        }
+
+        public void setPath(Path path) {
+            this.DrawingClassPath = path;
+        }
+
+        public Paint getPaint() {
+            return DrawingClassPaint;
+        }
+
+        public void setPaint(Paint paint) {
+            this.DrawingClassPaint = paint;
+        }
+    }
+    
 }
